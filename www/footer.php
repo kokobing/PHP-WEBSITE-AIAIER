9518d7961eace3375f12733b6974b2f053fbc84e
Koko lv <495105@qq.com>
2017-10-25 09:34:49 +0800

    rename dir path

---
+++ www/footer.php
@@ -0,0 +1,89 @@
+<div id="logobottom"><a href="http://aiaier.taobao.com" target="_blank"><img src="/upload/layout/<? echo getlayoutpic(1,0);?>"  /></a></div>
+
+
+
+<div id="footerbox" style=" position:relative">
+
+
+
+<div id="footerleft"></div>
+
+<div id="footermid">
+
+<? $footertxt=getlayoutinfo(1,'content');echo $footertxt[content];?>
+
+</div>
+
+
+<div id="footerright2" style="position:absolute;right:0px;bottom:0px;">
+
+<div id="footerrightimg"><a href="http://aiaier.taobao.com" target="_blank"><img src="/inc/pics/taobao.png" width="29" height="29" border="0" /></a>
+</div>
+
+<div id="footerrightimg"><a href="#"><img src="/inc/pics/imgs04.gif" width="29" height="29" border="0" /></a></div>
+
+<div id="footerrightimg"><a href="tencent://message/?uin=1729668216&Site=www.aiaier.com&Menu=yes"><img src="/inc/pics/imgs03.gif" width="29" height="29" border="0" /></a></div>
+
+<div id="footerrightimg"><a href="http://e.weibo.com/aalwedding/profile" target="_blank"><img src="/inc/pics/imgs02.gif" width="29" height="29" border="0" /></a></div>
+
+<div id="footerrightimg"><a href="http://www.dianping.com/shop/5548012" target="_blank"><img src="/inc/pics/imgs01.gif" width="29" height="29" border="0" /></a></div>
+
+
+
+</div>
+
+</div><!--end footerbox!-->
+
+
+
+
+<script>
+
+$('#footerrightimg img').each(function(){$(this).css("opacity",".5")});
+
+$('#footerrightimg img').mouseover(function()
+
+{
+
+	$(this).animate({
+
+	opacity:"1"
+
+	})
+
+}).mouseout(function()
+
+{
+
+	$(this).animate({
+
+		opacity:".5"
+
+	})
+
+});
+
+</script>
+
+
+
+<script type="text/javascript">
+
+swfobject.registerObject("FlashID");
+
+</script>
+
+
+
+<script type="text/javascript">
+
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fe13ef65c689a470300155ce1d526022a' type='text/javascript'%3E%3C/script%3E"));
+
+</script>
+
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F20b6dd9e9a2e5c3cd1095e97256aa78d' type='text/javascript'%3E%3C/script%3E"));
+</script>
---
+++ www/lifushow.php
@@ -0,0 +1,154 @@
+<?php
+require "./inc/config.php";
+require "./inc/function.class.php";
+
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="keywords" content="<?php echo $setinfo[keywords];?>" />
+<meta name="description" content="<?php echo $setinfo[description];?>" />
+<title><?php echo $setinfo[title];?></title>
+<link href="inc/css/css1.css" rel="stylesheet" type="text/css">
+<script src="/inc/js/jquery.min.js"></script>
+<script src="/inc/js/swfobject_modified.js" type="text/javascript"></script>
+<script src="/inc/js/jcarousellite_1.0.1.pack.js" type="text/javascript"></script>
+	<script type="text/javascript" src="inc/js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+	<link rel="stylesheet" type="text/css" href="inc/js/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+
+<!--[if lt IE 7]>
+        <script type="text/javascript" src="/inc/js/unitpngfix.js"></script>
+<![endif]-->
+
+<script type="text/javascript">
+
+
+
+   $(document).ready(function(){
+
+
+
+    $("#mainshow").jCarouselLite({
+
+
+
+        btnNext: "#righttarr",
+
+
+
+        btnPrev: "#leftarr",
+
+
+
+   auto: 8000,
+
+
+
+   scroll: 1,
+
+
+
+   speed: 300,
+
+
+
+   visible: 4
+
+
+
+    }); 
+
+
+
+   });
+
+
+
+</script>
+<?php if($setinfo[iscopy]=='1'){?>
+<script language="JavaScript">
+document.oncontextmenu=new Function("event.returnValue=false;");
+document.onselectstart=new Function("event.returnValue=false;");
+</script>
+<?php }?>
+<?php if($setinfo[otherheader]!=''){echo $setinfo[otherheader];}?>
+
+</head>
+<body>
+
+<? require "header.php"; ?>
+
+
+<div id="main_box">
+
+<div id="leftarr"><img src="inc/pics/arrleft.png" width="9" height="15" /></div>
+
+<div id="mainshow">
+
+<ul>
+
+
+
+<?
+
+$getnewslist=getnewslist($_GET[id],'id_newsinfo');
+
+for($i=0;$i<sizeof($getnewslist);$i++){
+
+	$allpic=getnewspicnuminfo($getnewslist[$i][id_newsinfo],2,'opicname as pic');
+
+?>
+
+<li><a href="/upload/news/<?=$allpic[1][pic];?>"  id="imgs0<?=$i;?>"><img src="/upload/news/<?=$allpic[0][pic];?>" width="200" height="348" /></a></li>
+
+<? }?>
+
+</ul>
+
+</div>
+
+<div id="righttarr"><img src="inc/pics/arrright.png" width="9" height="15" /></div>
+
+</div><!--end main_box!-->
+
+
+<? require "footer.php"; ?>
+    
+    
+
+    <script type="text/javascript">
+
+		$(document).ready(function() {
+
+
+
+			$("a[href$='.jpg']").fancybox(
+
+			
+
+								{
+
+        'transitionIn'  :   'elastic',
+
+        'transitionOut' :   'elastic',
+
+        'speedIn'       :   600, 
+
+        'speedOut'      :   200
+
+
+
+    }
+
+			);
+
+			
+
+		   });	
+
+		</script>	
+
+</body>
+</html>
